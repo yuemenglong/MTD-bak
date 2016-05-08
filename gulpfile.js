1d78de6e495b9907c8635daf34d3c6b2ca4e6655
menglong yue <menglong yue>
2016-05-08 19:27:34 +0800

    tmp

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,25 @@
 var gulp = require('gulp');
 var jadeToJsx = require("./gulp-jade-jsx");
 var jsxToJs = require("./gulp-jsx");
+var rename = require("gulp-rename");
+
+var del = require('del');
+var vinylPaths = require('vinyl-paths');
 
 gulp.task('default', function() {
     console.log("hello world");
 });
 
 gulp.task('jsx', function() {
-    gulp.src("src/*.js")
+    gulp.src("src/*.jsx")
         .pipe(jadeToJsx())
         .pipe(jsxToJs())
-        .pipe(gulp.dest("build"));
+        .pipe(rename({ extname: ".js" }))
+        .pipe(gulp.dest("src"));
 });
 
-gulp.task('build', function() {
-
+gulp.task('clean', function() {
+    gulp.src("src/*.jsx")
+        .pipe(rename({ extname: ".js" }))
+        .pipe(vinylPaths(del));
 });
--- src/svg.js
+++ src/svg.js
@@ -1,22 +1,31 @@
-var Bar = require("./bar");
-
+var Bar = require('./bar');
 var Svg = React.createClass({
-    getInitialState: function() {
-        return { lines: [], rects: [] };
+    getInitialState: function () {
+        return {
+            lines: [],
+            rects: []
+        };
     },
-    drawLine: function(x1, y1, x2, y2, clr) {
-        clr = clr || "#888";
+    drawLine: function (x1, y1, x2, y2, clr) {
+        clr = clr || '#888';
         var copy = this.state.lines.slice();
-        var key = `${x1}${y1}${x2}${y2}`;
+        var key = `${ x1 }${ y1 }${ x2 }${ y2 }`;
         var style = { stroke: clr };
-        copy.push({ x1: x1, y1: y1, x2: x2, y2: y2, key: key, style: style });
+        copy.push({
+            x1: x1,
+            y1: y1,
+            x2: x2,
+            y2: y2,
+            key: key,
+            style: style
+        });
         this.setState({ lines: copy });
     },
-    drawRect: function(x1, y1, x2, y2, clr, fill) {
-        clr = clr || "#888";
+    drawRect: function (x1, y1, x2, y2, clr, fill) {
+        clr = clr || '#888';
         fill = fill || false;
         var copy = this.state.rects.slice();
-        var key = `${x1}${y1}${x2}${y2}`;
+        var key = `${ x1 }${ y1 }${ x2 }${ y2 }`;
         var x = Math.min(x1, x2);
         var y = Math.min(y1, y2);
         var width = Math.max(x1, x2) - x;
@@ -27,27 +36,30 @@ var Svg = React.createClass({
         } else {
             style.fillOpacity = 0;
         }
-        copy.push({ x: x, y: y, width: width, height: height, key: key, style: style });
+        copy.push({
+            x: x,
+            y: y,
+            width: width,
+            height: height,
+            key: key,
+            style: style
+        });
         this.setState({ rects: copy });
     },
-    render: function() {
-        return html(`svg({...this.props})`).inner(function() {
-            this.state.lines.map(function(item) {
-                return html("line({...item})");
+    render: function () {
+        return React.createElement(
+            'svg',
+            this.props,
+            this.state.lines.map(function (item) {
+                return React.createElement('line', item);
+            }),
+            this.state.rects.map(function (item) {
+                return React.createElement('rect', item);
             })
-        }).and(function() {
-            this.state.rects.map(function(item) {
-                return html("rect({...item})");
-            });
-        });
+        );
     }
 });
-
-var svg = ReactDOM.render(
-    html("Svg(width='100', height='100')"),
-    document.body
-);
-
+var svg = ReactDOM.render(React.createElement(Svg, { width: '100', height: '100' }), document.body);
 svg.drawLine(20, 20, 80, 30);
 svg.drawRect(20, 20, 90, 90);
 var bar = new Bar(10, 10, 20, 20);
